b482ce85477c7663b6d9944c84cba1bf0812cd9a
Fatimatuj Johora Samrina <128919909+samrina2023@users.noreply.github.com>
2024-01-18 23:18:52 +0600

    Add files via upload

---
+++ basic1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int T,i,n;
+cout<<"enter the value of T:";
+cin>>T;
+for(i=1;i<=T;i++);
+cout<<"enter the value of n:";
+cin>>n;
+if (n%2==0){
+    cout<<"EVEN";
+}
+else{
+    cout<<"odd";
+}
+
+
+}
